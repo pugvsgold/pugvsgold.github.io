56394f91da77125fc3ca2d4c7c25a775b2a70c2a
pugvsgold <101931854+pugvsgold@users.noreply.github.com>
2022-08-28 18:00:35 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+
+
+let myimage = document.querySelector('img');
+myimage.onclick = function(){
+let mysrc = myimage.getAttribute('src');
+if(mysrc==='images/sample.png')
+{
+	myimage.setAttribute('src','images/sample2.png');
+}
+else
+{
+	myimage.setAttribute('src','images/sample.png');
+}
+}
+let myheading = document.querySelector('h1');
+let mybutton = document.querySelector('button');
+function setUserName() {
+ let myName = prompt('please enter your name');
+ if(!myName) {
+   setUserName();
+   } else {
+     localStorage.setItem('name', myName);
+     myheading.textContent = 'mozilla is cool'+ myName;
+   }  
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myheading.textContent = 'Mozilla is cool, ' + storedName;
+}
+mybutton.onclick = function() {
+  setUserName();
+}
+
