c244beb98176ea0d13fa82583981ef768ac366b3
pugvsgold <101931854+pugvsgold@users.noreply.github.com>
2022-08-29 19:10:54 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,34 @@ if(mysrc==='images/sample.png')
 {
 	myimage.setAttribute('src','images/sample2.png');
 }
+else if(mysrc==='images/sample2.png')
+{
+	myimage.setAttribute('src','images/carrr.png');
+}
+else if(mysrc==='images/carrr.png')
+{
+	myimage.setAttribute('src','images/emo3.png');
+}
+else if(mysrc==='images/emo3.png')
+{
+	myimage.setAttribute('src','images/Abstract_geometry.png');
+}
+else if(mysrc==='images/Abstract_geometry.png')
+{
+	myimage.setAttribute('src','images/sand_dunes.png');
+}
+else if(mysrc==='images/sand_dunes.png')
+{
+	myimage.setAttribute('src','images/interiorDesign.png');
+}
+else if(mysrc==='images/interiorDesign.png')
+{
+	myimage.setAttribute('src','images/ceiling_fan.png');
+}
+else if(mysrc==='images/ceiling_fan.png')
+{
+	myimage.setAttribute('src','images/apple.png');
+}
 else
 {
 	myimage.setAttribute('src','images/sample.png');
