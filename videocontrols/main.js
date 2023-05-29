fefa04572bfcc50eb04342026960363385cfa67c
pugvsgold <pugvsgold@gmail.com>
2023-05-29 19:49:50 +0530

    Update main.js

--- videocontrols/main.js
+++ videocontrols/main.js
@@ -13,7 +13,7 @@ myfunc();
 
 function myfunc() {
 let start = new Date(Date.now());
-let datee = new Date('May 29, 2023 19:48:00');
+let datee = new Date('May 29, 2023 19:53:00');
 
 while ( datee > start) {
 start = new Date(Date.now());
