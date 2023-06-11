686b12ec601023c39debcc8ff0bf814fad1b38eb
pugvsgold <pugvsgold@gmail.com>
2023-06-11 12:39:46 +0530

    Update main.js

--- videocontrols/main.js
+++ videocontrols/main.js
@@ -24,19 +24,4 @@ media.play();
 }
 }
 
-//media.play()
-  
-
-var video = document.querySelector('video');
-// use the whole window and a *named function*
-window.addEventListener('touchstart', function videoStart() {
-  video.play();
-  console.log('first touch');
-  // remove from the window and call the function we are removing
-  this.removeEventListener('touchstart', videoStart);
-});
-}
-
-
-
 
