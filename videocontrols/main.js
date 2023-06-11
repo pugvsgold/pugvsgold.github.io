c7c150d701655ef2333ff50eb13010914f18b5ec
pugvsgold <pugvsgold@gmail.com>
2023-06-11 10:29:08 +0530

    Update main.js

--- videocontrols/main.js
+++ videocontrols/main.js
@@ -1,8 +1,11 @@
 const media = document.querySelector("video");
-let hours = 19;
-let minutes = 1;
-let seconds = 1;
+let hours = prompt("Enter time in hours:  ");
+let minutes = prompt("Enter time in minutes:  ");
+//let seconds = prompt("Enter time in seconds:  ");
+let month = prompt("Enter month in text: ");
+let date = prompt("Enter date: ");
 
+//let sec = prompt("Enter time in seconds:  ");
 
 let myimage = document.querySelector('button');
 myimage.onclick = function(){
@@ -13,7 +16,9 @@ myfunc();
 
 function myfunc() {
 let start = new Date(Date.now());
-let datee = new Date('May 30, 2023 09:00:00');
+//let datee = new Date('May 29, 2023 19:15:00');
+let datee = new Date( month + ' ' + date +', 2023 ' + hours + ':' + minutes + ':00');
+console.log(datee);
 
 while ( datee > start) {
 start = new Date(Date.now());
