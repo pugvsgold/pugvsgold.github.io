9c5a5097783d0a8ecf12429ea8f257898141dbc3
pugvsgold <101931854+pugvsgold@users.noreply.github.com>
2023-09-27 20:44:51 +0530

    update

--- control/main.js
+++ control/main.js
@@ -1,14 +1,6 @@
 const media = document.querySelector("video");
 const audi = document.querySelector("audio");
 
-let start = new Date(Date.now());
-let hours = start.getHours();
-let minutes = prompt("Enter time in minutes to start");
-//let seconds = prompt("Enter time in seconds:  ");
-let month = start.getMonth();
-let date = start.getDate();
-let differ = 0;
-let diff = document.getElementById("difference");
 
 
 let myimage = document.querySelector('button');
@@ -17,8 +9,6 @@ let myimage = document.querySelector('button');
 
 
 window.addEventListener("DOMContentLoaded", () => {
-//let datee = new Date('May 29, 2023 19:15:00');
-let datee = new Date( month + 1 + ' ' + date +', 2023 ' + hours + ':' + minutes + ':00');
 
 
 myimage.onclick = function(){
@@ -37,7 +27,8 @@ websocket.addEventListener("message", ({ data }) => {
 	switch (evnt.type) {
 
 		case "playy":
-			media.play()
+			media.play();
+			audi.pause();
 	}
 });
 
