1ad94edac1ced811c5b17a09139aa8bdbe0a8de1
pugvsgold <101931854+pugvsgold@users.noreply.github.com>
2024-10-26 10:36:07 +0530

    Update main.js

--- logs/main.js
+++ logs/main.js
@@ -1,14 +1,30 @@
 window.addEventListener("DOMContentLoaded", () => {  
 
   const websocket = new WebSocket("ws://web-production-96ed5.up.railway.app:8001/");
-  
+
   const UA = navigator.userAgent;
   
   websocket.addEventListener("open", (ev) => {
-  websocket.send(JSON.stringify({"type": UA}));
+  websocket.send(JSON.stringify({"type": "send", "value": UA}));
   });
+  
+  document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+  websocket.send(JSON.stringify({"type": "recv"}));
+}
+
+const messages = document.createElement("ul");
+
+  document.body.appendChild(messages);
+  
+    websocket.onmessage = ({ data }) => {
+    const message = document.createElement("li");
+    const content = document.createTextNode(data);
+    message.appendChild(content);
+    messages.appendChild(message);
+  };
+
     
     
  });
-
-  
