b0ebe62331e3d613d9236c988c516b3923376d96
pugvsgold <101931854+pugvsgold@users.noreply.github.com>
2024-10-27 14:54:57 +0530

    Update main.js

--- logs/main.js
+++ logs/main.js
@@ -2,30 +2,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const websocket = new WebSocket("wss://web-production-96ed5.up.railway.app/");
 
-  const UA = navigator.userAgent;
-  const loc = Navigator.geolocation;
-  const uap = new UAParser();
-  uap.setUA(UA);
-  const result = uap.getResult();
-  const d = new Date();
-  
-  
-  websocket.addEventListener("open", (ev) => {
-  websocket.send(JSON.stringify({"type": "send", "value": UA, "Browser":result.browser, "Device": result.device, "OS": result.os, "engine": result.engine.name, "architecture": result.cpu.architecture + document.referrer + d}));
-  });
+
+
   
   document.getElementById("demo").onclick = function() {myFunction()};
 
 function myFunction() {
   websocket.send(JSON.stringify({"type": "recv"}));
 }
-//const vall = document.getElementById('inputstr').value;
-
-  document.getElementById("button").onclick = function() {myclick()};
-  function myclick() {
-    const inpp = document.querySelector('#inputstr').value;
-   websocket.send(JSON.stringify({"type": "send", "value": inpp}));
-}
+ 
 
 const messages = document.createElement("ul");
 
